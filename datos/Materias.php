e65cf0695df560f14b331a7be55a0762df5c75df
JDiegoRG <insa.juanrobles@gmail.com>
2022-04-27 06:29:33 -0500

    Arreglos 26/04

--- datos/Materias.php
+++ datos/Materias.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+error_reporting(0);
 include('include/config.php');
 include('include/checklogin.php');
 check_login();
@@ -9,15 +10,27 @@ check_login();
 <html lang="en">
 
 <head>
-	<title>Usuario | Principal</title>
+	<title> | </title>
 
 	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+	<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+	<link rel="stylesheet" href="assets/css/styles.css">
 	<link rel="stylesheet" href="assets/css/plugins.css">
+	<link rel="stylesheet" href="assets/css/themes/theme-6.css" id="skin_color" />
+
+
 </head>
-<body class="login" style="background: linear-gradient(50deg, darkblue, white, lightgreen);">
+
+<body style="background: #97e0e5;">
 	<div id="app">
 
 		<div class="app-content">
@@ -27,62 +40,41 @@ check_login();
 					<!-- start: PAGE TITLE -->
 					<section id="page-title">
 						<div class="row">
-							<div class="col-sm-8"> <br><br><br><br>
-								<h1 style="font-weight: 600;color: #000;">Usuario | Principal</h1>
+							<div class="col-sm-8">
+								<h1 class="mainTitle" style="color: #000;font-weight: 600;"> | Materias | </h1>
 							</div>
-						<br><br>
+							<ol class="breadcrumb">
+								<li>
+									<span>Administrador</span>
+								</li>
+								<li class="active">
+									<span>Inicio</span>
+								</li>
+							</ol>
 						</div>
 					</section>
-					<div class="container-fluid container-fullw bg-white">
-						<div class="row">
-							<div class="col-sm-4">
-								<div class="panel panel-white no-radius text-center">
-									<div class="panel-body" style="background: springgreen;font-weight: 600;">
-										<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i> </span>
-										<h2 style="font-weight: 600;color: #000;">Mi Perfil</h2>
-										<p class="links cl-effect-1">
-											<a href="">
-												Modificar Perfil
-											</a>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-4">
-								<div class="panel panel-white no-radius text-center">
-									<div class="panel-body" style="background: springgreen;font-weight: 600;">
-										<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-paperclip fa-stack-1x fa-inverse"></i> </span>
-										<h2 style="font-weight: 600;color: #000;">Materias</h2>
+					<!-- end: PAGE TITLE -->
+					<!-- start: BASIC EXAMPLE -->
+						<div class="container-fluid container-fullw bg-white">
+				         <?php
+                 include("search.php");
 
-										<p class="cl-effect-1">
-											<a href="Materias.php">
-												Asigne sus materias
+                  ?>
 											</a>
 										</p>
 									</div>
 								</div>
 							</div>
-							<div class="col-sm-4">
-								<div class="panel panel-white no-radius text-center">
-									<div class="panel-body" style="background: springgreen;font-weight: 600;">
-										<span class="fa-stack fa-2x"> <i class="fa fa-square fa-stack-2x text-primary"></i> <i class="fa fa-book fa-stack-1x fa-inverse"></i> </span>
-										<h2 style="font-weight: 600;color: #000;"> Horario y extras</h2>
 
-										<p class="links cl-effect-1">
-											<a href="">
-												consulte datos principales
-											</a>
-										</p>
-									</div>
-								</div>
-							</div>
 						</div>
 					</div>
-					<!-- end: SELECT BOXES -->
-
 				</div>
 			</div>
 		</div>
+
+<!-- start: SETTINGS -->
+
+		<!-- end: SETTINGS -->
 	</div>
 	<!-- start: MAIN JAVASCRIPTS -->
 	<script src="vendor/jquery/jquery.min.js"></script>
